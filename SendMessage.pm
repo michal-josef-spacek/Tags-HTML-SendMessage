f5e534c03b024f9c2876e8b8a53b1c363784a8e0
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-09-16 21:44:32 +0200

    Prepare for 0.09 version

--- SendMessage.pm
+++ SendMessage.pm
@@ -17,7 +17,7 @@ Readonly::Hash my %LANG => (
 	'send' => 'Send question',
 );
 
-our $VERSION = 0.01;
+our $VERSION = 0.09;
 
 # Constructor.
 sub new {
@@ -336,6 +336,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.01
+0.09
 
 =cut
